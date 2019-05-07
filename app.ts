d543624b81bb16855428af52a272c0cc81e33ad8
zhangyu1818 <723156735@qq.com>
2019-05-07 18:00:32 +0800

    update

--- app.ts
+++ app.ts
@@ -2,14 +2,26 @@ import dotenv from 'dotenv';
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-koa';
 import Koa from 'koa';
+import Router from 'koa-router';
+import session from 'koa-session';
 
 import connect from './src/db';
 import Schema from './src/graphql/schema';
 
 dotenv.config();
 
+const router = new Router();
+
+router.post(
+    '/login',
+    async (ctx): Promise<void> => {
+        console.log(ctx.session);
+    },
+);
 const runServer = async (): Promise<void> => {
     const app = new Koa();
+    app.keys = ['blog server secret hurr'];
+    app.use(session(app)).use(router.routes());
     await connect();
     const schema = await Schema();
     const server = new ApolloServer({
